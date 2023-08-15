09efb13805853acede2be1d40bff087ad23d1d59
Emrah Fidan <emrahfdn.work@gmail.com>
2023-08-16 00:59:10 +0300

    fixed errors

--- hoverboard/script.js
+++ hoverboard/script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
 const colors = ["#e76f51","#264653","#f4a261","#2a9d8f","#a2d2ff","#e63946","#5e548e","#6a4c93","#1982c4","#8ac926","#f72585","#ffbc42","#140152","#ccc9dc"]
-const SQUARES = 1034
+const SQUARES = 966
 
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
